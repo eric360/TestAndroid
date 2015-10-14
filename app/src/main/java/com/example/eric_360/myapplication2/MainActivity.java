42b2c1190cb39ecb8100dff2e87b087540c81275
eric360 <eric-richard.hongtuanha@360learning.com>
2015-10-14 21:11:47 +0200

    Test Working

--- app/src/main/java/com/example/eric_360/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/eric_360/myapplication2/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +19,12 @@ public class MainActivity extends AppCompatActivity {
     public View onCreateView(String name, Context context, AttributeSet attrs) {
         View createdView = super.onCreateView(name, context, attrs);
         return createdView;
+    }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
